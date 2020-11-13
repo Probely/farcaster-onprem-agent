c918b9e8508bbd2c3168de11dfbf1b857cbc8798
Joao Poupino <joao@probely.com>
2020-11-13 15:03:18 +0000

    Ensure that DNS requests work properly

--- bin/start-gateway.sh
+++ bin/start-gateway.sh
@@ -38,21 +38,19 @@ iptables -t nat -A FARCASTER-NAT -j MASQUERADE
 iptables -t nat -F POSTROUTING
 iptables -t nat -A POSTROUTING -j FARCASTER-NAT
 
-# Workaround for docker-compose having its own DNS resolver
-if $(grep -q "^nameserver[^0-9]*127.0.0.11" /etc/resolv.conf); then
-    rundir=/run/dnsmasq
-    lport=1053
-    mkdir -p ${rundir}
-    chown -R root:root ${rundir}
-    chmod 0711 ${rundir}
-    dnsmasq -x ${rundir}/dnsmasq.pid -p "${lport}" -i "${WG_GW_IF}"
-    for proto in tcp udp; do
-        iptables -t nat -I PREROUTING -i "${WG_GW_IF}" -p ${proto} \
-            --dport 53 -j REDIRECT --to-port "${lport}"
-        iptables -t filter -A INPUT -i "${WG_GW_IF}" -p ${proto} \
-            --dport "${lport}" -j ACCEPT
-    done
-fi
+# Redirect any DNS request to a local dnsmasq and let it handle the details
+rundir=/run/dnsmasq
+lport=1053
+mkdir -p ${rundir}
+chown -R root:root ${rundir}
+chmod 0711 ${rundir}
+dnsmasq -x ${rundir}/dnsmasq.pid -p "${lport}" -i "${WG_GW_IF}"
+for proto in tcp udp; do
+    iptables -t nat -I PREROUTING -i "${WG_GW_IF}" -p ${proto} \
+        --dport 53 -j REDIRECT --to-port "${lport}"
+    iptables -t filter -A INPUT -i "${WG_GW_IF}" -p ${proto} \
+        --dport "${lport}" -j ACCEPT
+done
 
 rc=1
 if start_wireguard "${WG_GW_IF}"; then
