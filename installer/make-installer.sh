af5563127ff617e3a72e38db3d2cb4120878ee65
Joao Poupino <joao@probely.com>
2021-07-21 14:22:26 +0000

    Use stock make-installer

--- installer/make-installer.sh
+++ installer/make-installer.sh
@@ -15,26 +15,15 @@ umask 077
 
 RUNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-genid() {
-    python -c "import base64, os; print(base64.b32encode(os.urandom(8)).decode().rstrip('=').lower())"
-}
-
-make_tmp_dir() {
-    local id=$(genid)
-    local tmp="${RUNDIR}/tmp/installer-${id}"
-    mkdir -p ${tmp}
-    echo ${tmp}
-}
-
 AGENTID=$(basename ${CONFIG_BUNDLE} | sed "s/onprem-\(\w*\)\.tar\.gz$/\1/")
-TMP_DIR=$(make_tmp_dir)
+TMP_DIR=$(mktemp -d "/tmp/installer-XXXXXXXX")
 
 mkdir -p "${TMP_DIR}/secrets/"
 tar -xpzvf "${CONFIG_BUNDLE}" --strip 1 -C "${TMP_DIR}/secrets/"
 cp ${RUNDIR}/setup.sh "${TMP_DIR}"
 cp ${RUNDIR}/../compose/docker-compose.tpl.yml "${TMP_DIR}/"
 
-cd ${RUNDIR} && mkdir -p ${RUNDIR}/target && ./makeself/makeself.sh --gzip --ssl-encrypt --nomd5 --nocrc \
+cd ${RUNDIR} && mkdir -p ${RUNDIR}/target && makeself --gzip --nomd5 --nocrc \
     --sha256 --license ${RUNDIR}/../LICENSE \
     ${TMP_DIR} \
     ${RUNDIR}/target/probely-onprem-agent-${AGENTID}.run \
